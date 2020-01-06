196425b1a2837b52a525352904f4feeb59afd80a
CubeTheThird <cubethethird@gmail.com>
2020-01-05 23:55:20 -0500

    Modified script to use pulseaudio sink instead. Simplified the code.

--- live_noise_filter.sh
+++ live_noise_filter.sh
@@ -6,6 +6,8 @@ workDir=$(mktemp -d)
 
 noiseFile="noise.wav"
 
+sinkName="filtered.audio"
+
 #record noise sample
 record()
 {
@@ -19,32 +21,24 @@ record()
 	paplay $noiseFile
 }
 
+quit()
+{
+	cd -
+	rm -rf "$workdir"
+	pactl unload-module module-null-sink
+	exit
+}
+
+
 cd $workDir
+trap quit SIGINT
 
-#detect if aloop module is loaded
-#allow the user to load the module here
-	if [ $(lsmod | grep -c snd_aloop) -eq 0 ]; then
-		echo "ALOOP module is not loaded."
-		read -p "Attempt to load the module?[y/n]" yn
-	    case $yn in
-	        [Yy]* ) sudo modprobe snd_aloop;;
-	        [Nn]* ) exit;;
-	        * ) echo "Please answer yes or no.";;
-	    esac
-	fi
-
-#get list of input and output devices (with details)
-	inputs=$(pactl list short sources | grep -E -v '(monitor|aloop)')
-	outputs=$(pactl list short sinks | grep aloop)
+#get list of input devices (with details)
+	inputs=$(pactl list short sources | grep -v monitor)
 
 #abort if devices aren't available
 	if [ $(echo "$inputs" | wc -l) -lt 1 ]; then
 		echo "No input devices detected. Aborting"
-		exit
-	fi
-
-	if [ $(echo "$outputs" | wc -l) -lt 1 ]; then
-		echo "No output devices detected. Aborting"
 		exit
 	fi
 
@@ -54,70 +48,42 @@ inputIndex=0
 		echo "Multiple input devices detected. Select from this list:"
 		echo "$inputs" | awk -F '\\s' '{print NR-1," ",$2}'
 		read -p "Enter an index:" inputIndex
-		exit
 	fi
 	inputs=$(echo "$inputs" | awk -v idx=$inputIndex '{if (NR-1 == idx) print}')
 
-outputIndex=0
-#allow user selection when multiple devices are available
-	if [ $(echo "$outputs" | wc -l) -gt 1 ]; then
-		echo "Multiple output devices detected. Select from this list:"
-		echo "$outputs" | awk -F '\\s' '{print NR-1," ",$2}'
-		read -p "Enter an index:" outputIndex
-		exit
-	fi
-	outputs=$(echo "$outputs" | awk -v idx=$outputIndex '{if (NR-1 == idx) print}')
-
 #load device names
 	input=$(echo "$inputs" | awk -F '\\s' '{print $2}')
-	output=$(echo "$outputs" | awk -F '\\s' '{print $2}')
-
 
 #get input device specs
 	format=$(echo "$inputs" | awk -F '\\s' '{print $4}')
+	echo $format
 	#number type
 	tmp=$(echo $format | grep -o "^[fus]")
 	case $tmp in
-		f ) inputEncoding="floating-point";;
-		s ) inputEncoding="signed-integer";;
-		u ) inputEncoding="unsigned-integer";;
+		f ) encoding="floating-point";;
+		s ) encoding="signed-integer";;
+		u ) encoding="unsigned-integer";;
 	esac
 	#bit count
-	inputBits=$(echo $format | grep -o "[0-9]*")
+	bits=$(echo $format | grep -o "[0-9]*")
 	#endianness
 	tmp=$(echo $format | grep -o "[bl]e$")
 	case $tmp in
-		be ) inputEndian="-B";;
-		le ) inputEndian="-L";;
+		be ) endian="-B";;
+		le ) endian="-L";;
 	esac
 	#channels
-	inputChannels=$(echo "$inputs" | awk -F '\\s' '{print $5}' | grep -o "[0-9]*")
+	channels=$(echo "$inputs" | awk -F '\\s' '{print $5}' | grep -o "[0-9]*")
 	#bitrate
-	inputBitrate=$(echo "$inputs" | awk -F '\\s' '{print $6}' | grep -o "[0-9]*")
+	sampleRate=$(echo "$inputs" | awk -F '\\s' '{print $6}' | grep -o "[0-9]*")
 
-
-#get output device specs
-	format=$(echo "$outputs" | awk -F '\\s' '{print $4}')
-	#number type
-	tmp=$(echo $format | grep -o "^[fus]")
-	case $tmp in
-		f ) outputEncoding="floating-point";;
-		s ) outputEncoding="signed-integer";;
-		u ) outputEncoding="unsigned-integer";;
-	esac
-	#bit count
-	outputBits=$(echo $format | grep -o "[0-9]*")
-	#endianness
-	tmp=$(echo $format | grep -o "[bl]e$")
-	case $tmp in
-		be ) outputEndian="-B";;
-		le ) outputEndian="-L";;
-	esac
-	#channels
-	outputChannels=$(echo "$outputs" | awk -F '\\s' '{print $5}' | grep -o "[0-9]*")
-	#bitrate
-	outputBitrate=$(echo "$outputs" | awk -F '\\s' '{print $6}' | grep -o "[0-9]*")
-
+#detect if the null sink has already been created, else create one
+	if [ $(pactl list short sinks | grep -c $sinkName) -eq 0 ]; then
+		echo "Creating new audio sink for output."
+		pactl load-module module-null-sink sink_name=$sinkName channels=$channels rate=$sampleRate format=$format sink_properties=device.description="Filtered_Audio_Sink"
+	else
+		echo "Re-using existing audio sink for output."
+	fi
 
 #record noise sample
 record
@@ -138,7 +104,4 @@ echo "Change recording port to <Loopback Analog Stereo Monitor> in PulseAudio to
 echo "Ctrl+C to terminate."
 
 #filter audio from $input to $output
-pacat -r -d $input --latency=1msec | sox -b $inputBits -e $inputEncoding -c $inputChannels -r $inputBitrate $inputEndian -t raw - -b $outputBits -e $outputEncoding -c $outputChannels -r $outputBitrate $outputEndian -t raw - noisered noise.prof 0.2 | pacat -p -d $output --latency=1msec
-
-cd -
-rm -rf "$workdir"
+pacat -r -d $input --latency=1msec | sox -b $bits -e $encoding -c $channels -r $sampleRate $endian -t raw - -t raw - noisered noise.prof 0.2 | pacat -p -d $sinkName --latency=1msec
